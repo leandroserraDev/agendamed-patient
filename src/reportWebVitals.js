e1d05908823e70fbd038faa3c9035134d86e0a5f
Leandro Serra <leandroserra.dba@hotmail.com>
2024-08-16 00:08:26 -0300

    .

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -3,9 +3,9 @@ const reportWebVitals = onPerfEntry => {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
-      getFCP(onPerfEntry);
+      getFCP(onPerfEntry); 
       getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getTTFB(onPerfEntry); 
     });
   }
 };
